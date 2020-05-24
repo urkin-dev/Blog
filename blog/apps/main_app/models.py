0ec45c8a0d630a7791aa119025e4a86b77c45e7e
Ivan <yurkin.ivan5456@gmail.com>
2020-05-24 18:52:47 +0300

    Added response home page design, made rander with articles, changed models

--- blog/apps/main_app/models.py
+++ blog/apps/main_app/models.py
@@ -14,15 +14,36 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
 
 class Article(models.Model):
-    article_image       = models.ImageField(upload_to='img/articles', max_length=100, default='img/default/default.jpg')
+    article_image       = models.ImageField(upload_to='img/articles', max_length=100, default='img/default_images/default.jpg')
     article_title       = models.CharField('Название статьи', max_length = 30)
-    article_description = models.CharField('Краткое описание статьи', max_length = 45)
+    article_description = models.CharField('Краткое описание статьи', max_length = 150)
     article_text        = models.TextField('Текст статьи')
     pub_date            = models.DateTimeField('Дата публикации')
-    is_main_article     = models.BooleanField('Главная статья(всего 3 места)', default=False)
+    is_main_in_category = models.BooleanField('Главная статья в своей категории(можно поставить только 1)', default=False)
+    is_main_in_homepage = models.BooleanField('Главная статья на домашней страницу(всего 3)', default=False)
     author_name         = models.CharField('Логин автора', max_length=50, default="No name")
     category            = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, db_column='category_no', default=0)
     likes               = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if self.is_main_in_category:
+            try:
+                temp = Article.objects.get(is_main_in_category=True, category=self.category)
+                if self != temp:
+                    temp.is_main_in_category = False
+                    temp.save()
+            except Article.DoesNotExist:
+                pass
+
+        if self.is_main_in_homepage:
+            try:
+                main_count = len(Article.objects.filter(is_main_in_homepage=True))
+                if main_count == 3:
+                    self.is_main_in_homepage = False
+            except Article.DoesNotExist:
+                pass
+            
+        super(Article, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.article_title
--- blog/apps/main_app/views.py
+++ blog/apps/main_app/views.py
@@ -1,8 +1,12 @@
 # TODO Create new pages for other categories
 from django.shortcuts import render
 
-from .models import Category
+from .models import Category, Article
 
 def index(req):
-    categories = Category.objects.all()
-    return render(req, 'pages/index.html', { 'categories': categories })
+    categories          = Category.objects.all()
+    main_articles       = Article.objects.filter(is_main_in_homepage=True).order_by('likes') # Get three main articles and order by likes
+    main_articles_array = main_articles[1:]
+
+    popular_articles = Article.objects.order_by('-likes')[:10]
+    return render(req, 'pages/index.html', { 'categories': categories, 'main_article': main_articles[0], 'articles': popular_articles, 'main_articles': main_articles_array })
